911426207f1310243595cdbb660e6a7d0c5fd81c
Zoe14127 <shajila.siricilla@gmail.com>
2026-09-10 18:06:17 -0500

    Personal Goals Display

---
+++ 9/3/PersonalGoalsDisplay.py
@@ -0,0 +1,7 @@
+color = input("What is your favorite color?: \n")
+animal = input("What is your favorite animal?: \n")
+food = input("What is your favorite food?: \n")
+
+print("Your favorite color is", color)
+print("Your favorite animal is", animal)
+print("Your favorite food is", food,end="")
